a1e2da0e429bad7a592b88e030e24a6c339baae5
nihala <nihalavk98@gmail.com>
2024-08-29 14:01:09 +0530

    comment

--- src/dr.physio/Home.jsx
+++ src/dr.physio/Home.jsx
@@ -5,12 +5,11 @@ import logo from './assets/drphysio_logo1.jpeg'
 import img1 from './assets/physio_img1.jpeg'
 import img2 from './assets/physio_aboutimg.jpg'
 import img3 from './assets/ortho_img.jpg'
-import img4 from './assets/neuro-img.jpg'
-import img5 from './assets/pediat-img.jpeg'
+import img4 from './assets/Neuro-Physio.jpg'
+import img5 from './assets/Child-Physiotherapy.jpg'
 import img6 from './assets/gery-img.jpg'
 import img7 from './assets/gynec-img.jpg'
 import img8 from './assets/surgical-img.jpg'
-import { IoReorderThreeOutline } from "react-icons/io5";
 import { IoCall } from "react-icons/io5";
 import { IoMailOpenOutline } from "react-icons/io5";
 import { MdOutlineAccessTime } from "react-icons/md";
@@ -34,7 +33,7 @@ function Home() {
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="m-auto" >
               <Nav.Link href="#home" className='nav-links'>Home</Nav.Link>
-              <Nav.Link href="#about" className='nav-links'>About Us</Nav.Link>
+              <Nav.Link href="#about-section"  className='nav-links'>About Us</Nav.Link>
               <NavDropdown title="Our Services" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1" className="nav-drop">Ortho Physiotherapy</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2" className="nav-drop">Neuro Physiotherapy</NavDropdown.Item>
@@ -43,78 +42,77 @@ function Home() {
                 <NavDropdown.Item href="#action/3.4" className="nav-drop"> Gynacological Physiotherapy</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.4" className="nav-drop">Post Surgical Physiotherapy</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="#about" className='nav-links'>Contact Us</Nav.Link>
+              <Nav.Link href="#contact" className='nav-links'>Contact Us</Nav.Link>
             </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
       <div className="main-body">
-         <div class="entry-image"> 
-           <img className='physio_img1' src={img1} /> 
-           <div className="caption">
-             <div className="caption1">Say Goodbye to Pain...</div> 
-             <div className="caption2">Welcome To Dr.Physio</div>
-           </div>
-          <div className="about">
-            
-              <div className="about-contents">
-                <img className='abt-images' src={img2} />
-                <div className="sub-headings">ABOUT US
-                <div className="abt-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas consectetur culpa nesciunt fugit officia earum veniam aperiam illo atque esse officiis quam quos magni, temporibus dolorum vel? Tempore, deleniti ex?</div>
-                </div>
+        <div class="entry-image"> 
+          <img className='physio_img1' src={img1} /> 
+          <div className="caption">
+            <div className="caption1">Say Goodbye to Pain...</div> 
+            <div className="caption2">Welcome To Dr.Physio</div>
+          </div>
+        <div className="about" id="about-section">    
+          <div className="about-contents">
+            <img className='abt-images' src={img2} />
+              <div className="sub-headings">ABOUT US
+                <div className="abt-para">At our center, we are dedicated to delivering result-oriented physiotherapy services grounded in scientific protocols. Our approach emphasizes advanced physiotherapy techniques and long-term rehabilitation strategies designed to ensure our clients achieve and maintain optimal functional independence throughout their lives. By focusing on personalized care and evidence-based practices, we aim to support our clients in their journey toward sustained health, mobility, and overall well-being.</div>
               </div>
-            <div className="about-head">Visions and Missions we have </div>
-            <div className="para">To emerge as a trust worthy medical rehabilitation institution, providing advanced physical therapy and rehabilitation services that enhance the quality of life for our patience and to deliver exceptional healthcare services by leveraging advanced technology and the expertise of our talented professionals, ensuring the highest quality of care for our patients.</div>
+          </div>
+          <div className="about-head">Visions and Missions we have </div>
+          <div className="para">To emerge as a trust worthy medical rehabilitation institution, providing advanced physical therapy and rehabilitation services that enhance the quality of life for our patience and to deliver exceptional healthcare services by leveraging advanced technology and the expertise of our talented professionals, ensuring the highest quality of care for our patients.</div>
+        </div>
+        <div className="services">
+          <div className="sub-headings">SERVICES WE PROVIDE</div>
+          <div className="service">
+          <Carousel>
+            <Carousel.Item>
+              <div className="service-images">
+                <img className='images' src={img3} />
+              </div>
+              <Carousel.Caption><h3 style={{color:"black"}}>Ortho Physiotherapy</h3></Carousel.Caption> 
+            </Carousel.Item>
+          <Carousel.Item>
+            <div className="service-images">
+              <img className='images' src={img4} />
             </div>
-          <div className="services">
-            <div className="sub-headings">SERVICES WE PROVIDE</div>
-            <div className="service">
-            <Carousel>
-              <Carousel.Item>
-                <div className="service-images">
-                  <img className='images' src={img3} />
-                </div>
-                <Carousel.Caption><h3 style={{color:"blue"}}>Ortho Physiotherapy</h3></Carousel.Caption> 
-              </Carousel.Item>
-              <Carousel.Item>
-                <div className="service-images">
-                  <img className='images' src={img4} />
-                </div>
-                <Carousel.Caption><h3 style={{color:"rgb(88, 88, 238)"}}>Neuro Physiotherapy</h3></Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <div className="service-images">
-                <img className='images' src={img5} />
-                </div>
-                <Carousel.Caption><h3 style={{color:"rgb(88, 88, 238)"}}>Pediatric Physiotherapy</h3></Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <div className="service-images">
-                  <img className='images' src={img6} />
-                </div>
-                <Carousel.Caption><h3 style={{color:"rgb(88, 88, 238)"}}>Geriatric Physiotherapy</h3></Carousel.Caption>
+            <Carousel.Caption><h3 style={{color:"black"}}>Neuro Physiotherapy</h3></Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="service-images">
+              <img className='images' src={img5} />
+            </div>
+            <Carousel.Caption><h3 style={{color:"black"}}>Pediatric Physiotherapy</h3></Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="service-images">
+              <img className='images' src={img6} />
+            </div>
+            <Carousel.Caption><h3 style={{color:"black"}}>Geriatric Physiotherapy</h3></Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
                 <div className="service-images">
                   <img className='images' src={img7} />
                 </div>
-                <Carousel.Caption><h3 style={{color:"rgb(88, 88, 238)"}}>Gynecological Physiotherapy</h3></Carousel.Caption>
+                <Carousel.Caption><h3 style={{color:"black"}}>Gynecological Physiotherapy</h3></Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
                 <div className="service-images">
                   <img className='images' src={img8} />
                 </div>
-                <Carousel.Caption><h3 style={{color:"rgb(88, 88, 238)"}}>Post Surgical Physiotherapy</h3></Carousel.Caption>
+                <Carousel.Caption><h3 style={{color:"black"}}>Post Surgical Physiotherapy</h3></Carousel.Caption>
               </Carousel.Item>
             </Carousel>
             </div>
           </div>
-          <div className="contact">
+          <div className="contact" id="contact">
             <div className="sub-headings">CONTACT US</div>
               <div className="connection">
                 <div className="contact-det">
                   <ul>
-                   <li><IoCall /></li>
+                   <li><IoCall style={{height:"45px"}}/></li>
                    <li><IoMailOpenOutline /></li>
                    <li><MdOutlineAccessTime /></li>
                    <li><CiLocationOn /></li>
@@ -122,9 +120,9 @@ function Home() {
                 </div>
                 <div className="contact-det">
                   <ul>
-                   <li>+91 9745224441</li>
+                   <li>+91 9745224441,<br></br>+91 8714440503</li>
                    <li>drphysio@gmail.com</li>
-                   <li>9.00AM-1.30PM (Mon-Sat)</li>
+                   <li>9.00AM-1.00PM (Mon-Sat)</li>
                    <li>Sahara Building, Beach Road Koyilandy</li>
                   </ul>
                 </div>
@@ -134,13 +132,13 @@ function Home() {
           <div className="foot">
               <div className="icons">
               <ul>
-                <li className='icon1'><FaWhatsapp /></li>
-                <li className='icon1'><FaSquareInstagram /></li>
-                <li className='icon1'><IoMailOpenOutline /></li>
+                <li ><FaWhatsapp className='icon1'/></li>
+                <li ><FaSquareInstagram className='icon1'/></li>
+                <li ><IoMailOpenOutline className='icon1'/></li>
               </ul>
             
             </div>
-            <div className="foot-discr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae cum exercitationem, quas dolor, eligendi voluptate id deleniti nostrum est natus tenetur</div>
+            <div><h5  className="foot-discr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae cum exercitationem, quas dolor, eligendi voluptate id deleniti nostrum est natus tenetur</h5></div>
           </div>
         </div>
     </div>
